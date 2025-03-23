12d7bee093082bb4a485a443b9a919e7295ba58f
Phu An Nguyen <150226832+Savoxism@users.noreply.github.com>
2025-03-23 12:10:47 +0700

    Update

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
-from vnstock3 import Vnstock
+from vnstock import Vnstock
 
-symbol = 'HAG'
+symbol = 'FPT'
 stock = Vnstock().stock(symbol=symbol, source='TCBS')
 
 # print(stock.quote.history(start='2024-03-15', end='2024-06-23'))
@@ -8,14 +8,14 @@ stock = Vnstock().stock(symbol=symbol, source='TCBS')
 # print(stock.finance.cash_flow(period='quarter', lang='vi'))
 # print(stock.finance.ratio(period='quarter', lang='vi'))
 
-df_quote_history= stock.quote.history(start='2025-01-01', end='2025-01-31')
-df_cash_flow = stock.finance.cash_flow(period='quarter') 
-df_income_statement = stock.finance.income_statement(period='quarter')
-df_ratio = stock.finance.ratio(period='quarter')
+df_quote_history= stock.quote.history(start='2018-01-01', end='2025-01-31')
+# df_cash_flow = stock.finance.cash_flow(period='quarter') 
+# df_income_statement = stock.finance.income_statement(period='quarter')
+# df_ratio = stock.finance.ratio(period='quarter') 
 
-df_quote_history.to_csv(f'files/{symbol}_2025_quote_history.csv', index=False)
-df_cash_flow.to_csv(f'files/{symbol}_2025_cash_flow.csv', index=False)
-df_income_statement.to_csv(f'files/{symbol}_2025_income_statement.csv', index=False)
-df_ratio.to_csv(f'files/{symbol}_2025_ratio.csv', index=False)
+df_quote_history.to_csv(f'new/{symbol}_quote_history.csv', index=False)
+# df_cash_flow.to_csv(f'new/{symbol}_cash_flow.csv', index=False)
+# df_income_statement.to_csv(f'new/{symbol}_income_statement.csv', index=False)
+# df_ratio.to_csv(f'new/{symbol}_ratio.csv', index=False)
 print('done!')
 
